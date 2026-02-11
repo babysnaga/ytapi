d2abdac6a08ea0bdf3c027ab87b56849eb3e3eaf
babysnaga <marcosguilherme-lima@hotmail.com>
2026-02-11 10:43:01 -0300

    set cors for lovable

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://babypinmanager.lovable.app/"],  # depois a gente restringe pro seu domínio do Lovable
+    allow_origins=["https://babypinmanager.lovable.app"],  # depois a gente restringe pro seu domínio do Lovable
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
